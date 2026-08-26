7568f59e1f4441f6d49664f26e7b237d0d5f2757
Mark Heck <mheck83@gmail.com>
2026-08-26 02:33:40 -0500

    Poison S05 dependency direction

--- src/domain/s05-canary.ts
+++ src/domain/s05-canary.ts
@@ -1,3 +1,5 @@
+import type { S05ApplicationCanary } from "../application/s05-canary.js";
 export interface S05DomainCanary {
   readonly value: string;
+  readonly application?: S05ApplicationCanary;
 }
